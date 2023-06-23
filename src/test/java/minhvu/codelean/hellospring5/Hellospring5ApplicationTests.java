698209886d5b358f112e3e66b5c3f38247fbf650
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2023-06-23 13:40:12 +0700

    hellospring

---
+++ src/test/java/minhvu/codelean/hellospring5/Hellospring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package minhvu.codelean.hellospring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hellospring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
